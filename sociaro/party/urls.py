3e56cadb39202ff9a32aa4f548b7b07b6b3790a1
repair-cat <simf61@yandex.ru>
2021-12-13 00:52:09 +0500

    добавлен вложенный роут для плейлиста

--- sociaro/party/urls.py
+++ sociaro/party/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from rest_framework_nested import routers
 from rest_framework.routers import SimpleRouter
 
-from .views import get_and_suggest_track, PartyView, RecommendTracksView
+from .views import get_and_suggest_track, PartyView, RecommendTracksView, PlayTracksView
 
 router = SimpleRouter(trailing_slash=False)
 
 router.register('parties', PartyView, basename='parties')
 party_router = routers.NestedSimpleRouter(router, 'parties', lookup='party')
 party_router.register('recommend', RecommendTracksView, basename='recommend')
+party_router.register('playlist', PlayTracksView, basename='playlist')
 
 urlpatterns = [
     path('search-track', get_and_suggest_track)
