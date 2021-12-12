7b4611965f499c77301804c22335b2af1cfeb31a
repair-cat <simf61@yandex.ru>
2021-12-13 00:53:16 +0500

    переопределены методы create и perform_create для рекомендаций и треков плейлиста

--- sociaro/party/views.py
+++ sociaro/party/views.py
@@ -8,6 +8,7 @@ from .models import Track, Party, RecommendTrack, PlaylistTrack
 from .permissions import PartyOwner
 from .serializers import PartySerializer, RecommendTrackSerializer, PlayTrackSerializer
 from datetime import datetime
+from django.views.decorators.cache import cache_page
 
 
 class PartyView(viewsets.ModelViewSet):
@@ -50,10 +51,51 @@ class RecommendTracksView(viewsets.ModelViewSet, LikedMixin):
     def get_queryset(self):
         return RecommendTrack.objects.filter(party=self.kwargs['party_pk']).order_by('likes')
 
+    def create(self, request, *args, **kwargs):
+        # сравниваем время и дату, указанные в поле closed Мероприятия с текущими временем и датой
+        party_time = Party.objects.get(id=int(self.kwargs['party_pk'])).closed
+        if datetime.now().timestamp() < party_time.timestamp():
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response({"error": "Прием заявок окончен"})
+
+    def perform_create(self, serializer):
+        serializer.save(party=Party.objects.get(id=int(self.kwargs['party_pk'])))
+
+
+class PlayTracksView(viewsets.ModelViewSet):
+    """
+    Для доступа к трекам в плейлисте через мероприятие
+    """
+    serializer_class = PlayTrackSerializer
+
+    def get_queryset(self):
+        return PlaylistTrack.objects.filter(party=self.kwargs['party_pk'])
+
+    def create(self, request, *args, **kwargs):
+        # находим значение максимально допустимого кол-ва треков в плейлисте мероприятия
+        # и кол-во уже добавленных треков
+        max_qty_tracks = Party.objects.get(id=int(self.kwargs['party_pk'])).max_qty_tracks
+        playlist_len = PlaylistTrack.objects.filter(party=int(self.kwargs['party_pk'])).count()
+        if playlist_len < max_qty_tracks:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response({"error": "Плейлист заполнен"})
+
+    def perform_create(self, serializer):
+        serializer.save(party=Party.objects.get(id=int(self.kwargs['party_pk'])))
+
 
 @api_view(['GET', 'POST'])
 @authentication_classes([authentication.TokenAuthentication])
 @permission_classes([IsAuthenticated])
+@cache_page(60 * 15)    # 15 мин тайм-аут кэширования
 def get_and_suggest_track(request):
     """
     Найти трек.
