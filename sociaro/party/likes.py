7fd9dfd6e98f1c6950edf529b12dd6ea7124b2a4
repair-cat <simf61@yandex.ru>
2021-12-16 11:05:07 +0500

    убран лишний код

--- sociaro/party/likes.py
+++ sociaro/party/likes.py
@@ -1,9 +1,5 @@
-# функционал добавления голоса
-from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from .models import Like
-
-User = get_user_model()
 
 
 def add_like(obj, user):
