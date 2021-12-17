1a64b12e516a426eb5bb05bb29ec401e26e41584
repair-cat <simf61@yandex.ru>
2021-12-17 00:58:08 -0800

    logging settings added

--- sociaro/sociaro/settings.py
+++ sociaro/sociaro/settings.py
@@ -147,15 +147,30 @@ SWAGGER_SETTINGS = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(name)-12s %(levelname)-8s %(message)s'
+        },
+        'file': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'console'
         },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': 'sociaro.log'
+        }
     },
     'loggers': {
         '': {
-            'handlers': ['console'],
             'level': 'DEBUG',
-        },
-    },
+            'handlers': ['console', 'file']
+        }
+    }
 }
